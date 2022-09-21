9bae6252711dfdfa130db3eb39dd5e5b8c6625a5
Weiming8592 <110385057+Weiming8592@users.noreply.github.com>
2022-09-21 11:41:31 +0800

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -4,9 +4,9 @@
 date=$(date +%d_%m_%Y_%H:%M:%S)
 mkdir -p /root/Downloads/${date}
 mkdir -p /root/tmp/"Unzip"+${date}
-cd tmp
 
 #Download the zip file from the IP2Location website
+cd tmp
 wget -O ${date}.zip -q "https://www.ip2location.com/download?token=vzLavs4LYMDCSslndae9S9vblYJEn71hqKRq3clqVCjIBQdwn1UN4wOfF20f9ZKY&file=db3bin"
 if [ ! -f ${date}.zip ]; then
         echo "Download failed."
@@ -39,15 +39,19 @@ fi
 #Copy the unzip BIN file into a folder in the Downloads directory 
 cd "Unzip"+${date}/
 cp IP-COUNTRY-REGION-CITY.BIN /root/Downloads/${date}
-
-#Rename the folder based on the created or last modification date of the file
-cd ~
-cd Downloads
-create_date=$(stat -c '%w' ${date}) 
-mv ${date} ${create_date}
-if [ -z "$( find . -name '${create_date}' )" ]; then
-        echo "Rename Failed"
-	exit 0
+if [ -z "$( find . -name 'IP-COUNTRY-REGION-CITY.BIN' )" ]; then
+        echo "Copy Failed"
+        exit 0
 else
-	echo "Rename Successful"
+        echo "Copy Successful"
 fi
+
+#Remove the zip file and unzip folder in the tmp directory
+cd ../ 
+rm -rf ${date}.zip "Unzip"+${date}
+if [ -z "$( find . -name 'IP-COUNTRY-REGION-CITY.BIN' )" ]; then
+        echo "Remove Successful"
+else
+        echo "Remove Failed"
+	exit 0
+fi 
